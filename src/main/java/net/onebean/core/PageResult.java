aa2a662d1f4fb9cffd00b2295d7a80c8c5dba919
0neBean <oneb1n@foxmail.com>
2018-04-12 14:12:39 +0800

    Initial commit

---
+++ src/main/java/net/onebean/core/PageResult.java
@@ -0,0 +1,43 @@
+package net.onebean.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PageResult<T> {
+    /**
+     * 分页对象
+     */
+    private Pagination pagination;
+    /**
+     * 请求成功标识
+     */
+    private Boolean flag;
+    /**
+     * 返回的列表
+     */
+    private List<T> data = new ArrayList<T>(0);
+
+    public Pagination getPagination() {
+        return pagination;
+    }
+
+    public void setPagination(Pagination pagination) {
+        this.pagination = pagination;
+    }
+
+    public Boolean getFlag() {
+        return flag;
+    }
+
+    public void setFlag(Boolean flag) {
+        this.flag = flag;
+    }
+
+    public List<T> getData() {
+        return data;
+    }
+
+    public void setData(List<T> data) {
+        this.data = data;
+    }
+}
