4d5bfb595d4ef09dac595211e7f82c9a2601b337
0neBean <oneb1n@foxmail.com>
2018-04-12 14:39:41 +0800

    fix package name worng

--- src/main/java/net/onebean/core/extend/DynamicMapperSqlSessionFactoryBean.java
+++ src/main/java/net/onebean/core/extend/DynamicMapperSqlSessionFactoryBean.java
@@ -97,7 +97,7 @@ public class DynamicMapperSqlSessionFactoryBean extends SqlSessionFactoryBean {
 		try {
 			Resource[] rs_model = resolver.getResources("classpath*:net/onebean/**/model/**/*.class");
 			// by tangmingbao 为了支持移动端数据上报 而添加，此处model的匹配规则应该是可配置
-			Resource[] rs_entity = resolver.getResources("classpath*:netW/onebean/**/entity/**/*.class");
+			Resource[] rs_entity = resolver.getResources("classpath*:net/onebean/**/entity/**/*.class");
 			
 			Resource[] rs=(Resource[]) ArrayUtils.addAll(rs_model,rs_entity);
 			
